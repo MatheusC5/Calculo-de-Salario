0e2d9dbd72d46f13eaf7a7d4743aeabd0230db36
Matheus-C5 <matheushcordeiro55@gmail.com>
2023-03-24 07:26:54 -0300

    Projeto concluído

---
+++ CalcularSalario/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CalcularSalario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtHorasExtras = new System.Windows.Forms.TextBox();
+            this.LblHorasExtras = new System.Windows.Forms.Label();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.LblCalculoSalario = new System.Windows.Forms.Label();
+            this.TxtSalarioMes = new System.Windows.Forms.TextBox();
+            this.TxtValorHora = new System.Windows.Forms.TextBox();
+            this.TxtHorasTrabalhadas = new System.Windows.Forms.TextBox();
+            this.BtnCalcularSalario = new System.Windows.Forms.Button();
+            this.LblValorHora = new System.Windows.Forms.Label();
+            this.LblHorasTrabalhadas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtHorasExtras
+            // 
+            this.TxtHorasExtras.Location = new System.Drawing.Point(421, 184);
+            this.TxtHorasExtras.Name = "TxtHorasExtras";
+            this.TxtHorasExtras.Size = new System.Drawing.Size(154, 22);
+            this.TxtHorasExtras.TabIndex = 40;
+            this.TxtHorasExtras.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtHorasExtras_KeyDown);
+            this.TxtHorasExtras.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtHorasExtras_KeyPress);
+            // 
+            // LblHorasExtras
+            // 
+            this.LblHorasExtras.AutoSize = true;
+            this.LblHorasExtras.Location = new System.Drawing.Point(225, 184);
+            this.LblHorasExtras.Name = "LblHorasExtras";
+            this.LblHorasExtras.Size = new System.Drawing.Size(87, 16);
+            this.LblHorasExtras.TabIndex = 39;
+            this.LblHorasExtras.Text = "Horas Extras:";
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(59)))), ((int)(((byte)(62)))));
+            this.BtnLimpar.Location = new System.Drawing.Point(228, 301);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(145, 49);
+            this.BtnLimpar.TabIndex = 38;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = false;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // LblCalculoSalario
+            // 
+            this.LblCalculoSalario.AutoSize = true;
+            this.LblCalculoSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCalculoSalario.Location = new System.Drawing.Point(310, 33);
+            this.LblCalculoSalario.Name = "LblCalculoSalario";
+            this.LblCalculoSalario.Size = new System.Drawing.Size(199, 29);
+            this.LblCalculoSalario.TabIndex = 37;
+            this.LblCalculoSalario.Text = "Calcular Salário";
+            // 
+            // TxtSalarioMes
+            // 
+            this.TxtSalarioMes.Enabled = false;
+            this.TxtSalarioMes.Location = new System.Drawing.Point(315, 395);
+            this.TxtSalarioMes.Name = "TxtSalarioMes";
+            this.TxtSalarioMes.Size = new System.Drawing.Size(154, 22);
+            this.TxtSalarioMes.TabIndex = 36;
+            // 
+            // TxtValorHora
+            // 
+            this.TxtValorHora.Location = new System.Drawing.Point(421, 242);
+            this.TxtValorHora.Name = "TxtValorHora";
+            this.TxtValorHora.Size = new System.Drawing.Size(154, 22);
+            this.TxtValorHora.TabIndex = 35;
+            this.TxtValorHora.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtValorHora_KeyDown);
+            this.TxtValorHora.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtValorHora_KeyPress);
+            // 
+            // TxtHorasTrabalhadas
+            // 
+            this.TxtHorasTrabalhadas.Location = new System.Drawing.Point(421, 123);
+            this.TxtHorasTrabalhadas.Name = "TxtHorasTrabalhadas";
+            this.TxtHorasTrabalhadas.Size = new System.Drawing.Size(154, 22);
+            this.TxtHorasTrabalhadas.TabIndex = 34;
+            this.TxtHorasTrabalhadas.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtHorasTrabalhadas_KeyDown);
+            this.TxtHorasTrabalhadas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtHorasTrabalhadas_KeyPress);
+            // 
+            // BtnCalcularSalario
+            // 
+            this.BtnCalcularSalario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(255)))), ((int)(((byte)(74)))));
+            this.BtnCalcularSalario.Location = new System.Drawing.Point(421, 301);
+            this.BtnCalcularSalario.Name = "BtnCalcularSalario";
+            this.BtnCalcularSalario.Size = new System.Drawing.Size(145, 49);
+            this.BtnCalcularSalario.TabIndex = 33;
+            this.BtnCalcularSalario.Text = "Calcular Salário";
+            this.BtnCalcularSalario.UseVisualStyleBackColor = false;
+            this.BtnCalcularSalario.Click += new System.EventHandler(this.BtnCalcularSalario_Click);
+            // 
+            // LblValorHora
+            // 
+            this.LblValorHora.AutoSize = true;
+            this.LblValorHora.Location = new System.Drawing.Point(225, 242);
+            this.LblValorHora.Name = "LblValorHora";
+            this.LblValorHora.Size = new System.Drawing.Size(98, 16);
+            this.LblValorHora.TabIndex = 32;
+            this.LblValorHora.Text = "Valor por Hora:";
+            // 
+            // LblHorasTrabalhadas
+            // 
+            this.LblHorasTrabalhadas.AutoSize = true;
+            this.LblHorasTrabalhadas.Location = new System.Drawing.Point(225, 123);
+            this.LblHorasTrabalhadas.Name = "LblHorasTrabalhadas";
+            this.LblHorasTrabalhadas.Size = new System.Drawing.Size(128, 16);
+            this.LblHorasTrabalhadas.TabIndex = 31;
+            this.LblHorasTrabalhadas.Text = "Horas Trabalhadas:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(255)))), ((int)(((byte)(244)))));
+            this.ClientSize = new System.Drawing.Size(842, 457);
+            this.Controls.Add(this.TxtHorasExtras);
+            this.Controls.Add(this.LblHorasExtras);
+            this.Controls.Add(this.BtnLimpar);
+            this.Controls.Add(this.LblCalculoSalario);
+            this.Controls.Add(this.TxtSalarioMes);
+            this.Controls.Add(this.TxtValorHora);
+            this.Controls.Add(this.TxtHorasTrabalhadas);
+            this.Controls.Add(this.BtnCalcularSalario);
+            this.Controls.Add(this.LblValorHora);
+            this.Controls.Add(this.LblHorasTrabalhadas);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtHorasExtras;
+        private System.Windows.Forms.Label LblHorasExtras;
+        private System.Windows.Forms.Button BtnLimpar;
+        private System.Windows.Forms.Label LblCalculoSalario;
+        private System.Windows.Forms.TextBox TxtSalarioMes;
+        private System.Windows.Forms.TextBox TxtValorHora;
+        private System.Windows.Forms.TextBox TxtHorasTrabalhadas;
+        private System.Windows.Forms.Button BtnCalcularSalario;
+        private System.Windows.Forms.Label LblValorHora;
+        private System.Windows.Forms.Label LblHorasTrabalhadas;
+    }
+}
+
---
+++ CalcularSalario/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcularSalario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            // limpar os campos se estiverem preenchidos
+            if (TxtHorasTrabalhadas.Text != "" || TxtHorasExtras.Text != ""
+                || TxtValorHora.Text != "" || TxtSalarioMes.Text != "")
+            {
+                TxtHorasTrabalhadas.Clear();
+                TxtHorasExtras.Clear();
+                TxtValorHora.Clear();
+                TxtSalarioMes.Clear();
+
+                TxtHorasTrabalhadas.Focus();
+            }
+
+            // quando tudo estiver vazio, o campo das horas trabalhadas ficará selecionado
+            else
+            {
+                TxtHorasTrabalhadas.Focus();
+            }
+        }
+
+        private void BtnCalcularSalario_Click(object sender, EventArgs e)
+        {
+            // declarar variáveis
+            int HorasTrabalhadas, HorasExtras;
+            double ValorHora, SalarioMes;
+
+            // o valor das horas trabalhadas e o valor por hora ficarem vazios
+            if (TxtHorasTrabalhadas.Text == "" && TxtValorHora.Text == "")
+            {
+                MessageBox.Show("O valor das horas trabalhadas e o valor por hora são obrigatórios!",
+                    "Dados Vazios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            // o valor das horas trabalhadas ficar vazio
+            else if (TxtHorasTrabalhadas.Text == "")
+            {
+                MessageBox.Show("O valor das horas trabalhadas é obrigatório!", "Dados Vazios",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            // o valor por hora ficar vazio
+            else if (TxtValorHora.Text == "")
+            {
+                MessageBox.Show("O valor por hora é obrigatório!", "Dados Vazios",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            // mostrar mensagem para confirmar o cálculo do salário com as horas extras
+            else if (TxtHorasExtras.Text != "" && MessageBox.Show(this, "Deseja realmente calcular o salário do mês?", "Confirmação",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                // converter as horas trabalhadas para int
+                HorasTrabalhadas = Convert.ToInt32(TxtHorasTrabalhadas.Text);
+
+                // converter as horas extras para int
+                HorasExtras = Convert.ToInt32(TxtHorasExtras.Text);
+
+                // converter o valor por hora para int
+                ValorHora = Convert.ToDouble(TxtValorHora.Text);
+
+                // calcular o salário do mês
+                SalarioMes = HorasTrabalhadas * ValorHora + HorasExtras * ValorHora * 1.5;
+
+                // mostrar mensagem de que o cálculo do salário deu certo
+                MessageBox.Show("O Salário foi calculado com sucesso!");
+
+                // mostrar o salário do mês
+                TxtSalarioMes.Text = "R$ " + SalarioMes.ToString("F2");
+            }
+
+            // mostrar mensagem para confirmar o cálculo do salário sem as horas extras
+            else if (TxtHorasExtras.Text == "" && MessageBox.Show(this, "Deseja realmente calcular o salário do mês?", "Confirmação",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                // converter as horas trabalhadas para int
+                HorasTrabalhadas = Convert.ToInt32(TxtHorasTrabalhadas.Text);
+
+                // converter o valor por hora para int
+                ValorHora = Convert.ToDouble(TxtValorHora.Text);
+
+                // calcular o salário do mês
+                SalarioMes = HorasTrabalhadas * ValorHora;
+
+                // mostrar mensagem de que o cálculo do salário deu certo
+                MessageBox.Show("O Salário foi calculado com sucesso!");
+
+                // mostrar o salário do mês
+                TxtSalarioMes.Text = "R$ " + SalarioMes.ToString("F2");
+            }
+        }
+
+        private void TxtHorasTrabalhadas_KeyDown(object sender, KeyEventArgs e)
+        {
+            // pressionar a tecla enter sem preencher as horas trabalhadas não vai pro campo de horas extras
+            if (TxtHorasTrabalhadas.Text == "" && e.KeyCode == Keys.Enter)
+            {
+                TxtHorasTrabalhadas.Focus();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+
+            // pressionar a tecla enter quando preencher as horas trabalhadas vai pro campo de horas extras
+            else if (TxtHorasTrabalhadas.Text != "" && e.KeyCode == Keys.Enter)
+            {
+                TxtHorasExtras.Focus();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void TxtHorasExtras_KeyDown(object sender, KeyEventArgs e)
+        {
+            // pressionar a tecla enter sem preencher as horas extras vai pro campo de valor por hora
+            if (TxtHorasExtras.Text == "" && e.KeyCode == Keys.Enter)
+            {
+                TxtValorHora.Focus();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+
+            // pressionar a tecla enter quando preencher as horas extras vai pro campo de valor por hora
+            else if (TxtHorasExtras.Text != "" && e.KeyCode == Keys.Enter)
+            {
+                TxtValorHora.Focus();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void TxtValorHora_KeyDown(object sender, KeyEventArgs e)
+        {
+            // pressionar a tecla enter sem preencher o valor por hora não vai exibir a mensagem de confirmação
+            if (TxtValorHora.Text == "" && e.KeyCode == Keys.Enter)
+            {
+                TxtValorHora.Focus();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+
+            // pressionar a tecla enter quando preencher o valor por hora vai exibir a mensagem de confirmação
+            else if (TxtValorHora.Text != "" && e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                BtnCalcularSalario.PerformClick();
+            }
+        }
+
+        private void TxtHorasTrabalhadas_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // permitir somente números para preencher as horas trabalhadas
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void TxtHorasExtras_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // permitir somente números para preencher as horas extras
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void TxtValorHora_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // permitir , para preencher o valor por hora
+            if (e.KeyChar == ',' && !((TextBox)sender).Text.Contains(','))
+            {
+
+            }
+
+            // permitir números para preencher o valor por hora
+            else
+            {
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            }
+        }
+    }
+}
